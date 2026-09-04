93c8dab0c3f8273af9650442e57c7d81582aa02f
Aiden Fine <aiden03fine@gmail.com>
2026-09-04 18:25:46 -0400

    simple lex for sql query

---
+++ pkg/sql/lex/lex.go
@@ -0,0 +1,134 @@
+package lex
+
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+// SQL examples
+// SELECT id, name FROM users WHERE age > 18
+// After lex
+// [SELECT, ID(id), COMMA, ID(name), FROM, ID(users), WHERE, ID(age), GT(>), INT(18)]
+
+type tokenType int
+
+// Defines the token types for the lex. With a defined token struct we could simply
+// use the TokenType like this.
+//
+//	type Token struct {
+//		Type TokenType
+//		Value string
+//	}
+//
+// This makes it super easy to be used like
+//
+//	token := Token{
+//		Type: SELECT,
+//		Value: "SELECT",
+//	}
+//
+// and for a query like `SELECT users FROM accounts WHERE id > 10`
+//
+// We can easily id which table's to query from because Token will be something like...
+//
+//	Token{
+//		Type: ID
+//		Value: "accounts",
+//	}
+const (
+	SELECT tokenType = iota
+	FROM
+	WHERE
+
+	ID
+	INT
+	STRING
+
+	EQ
+	GT
+	LT
+
+	COMMA
+	STAR
+)
+
+type Token struct {
+	Type  tokenType
+	Value string
+}
+
+// the lexer should take in a sql string and return a list of `Tokens`
+
+// lexer should go char by char until it hits a whitespace or something that indicates a token has ended.
+// So for "SELECT" it would go by each char and since SELECT is not an identifier we can assume that is a finished token
+//
+// Now for identifiers if we take the sql query `SELECT users FROM accounts WHERE id > 10`
+// Our identifiers (ID) would be users, accounts, id, 10.
+//
+
+// lex will take in a sql query and return a []Token. As of now this is very loosely created and will really take anything in.
+// We should make this much more strict in the future. One example is if we take `SELECT !!!` the lex will
+// Just assume !!! is valid.
+// TOOD: make this more strict
+func Lex(sql string) []Token {
+	currStr := ""
+	tokens := []Token{}
+
+	flush := func() {
+		if currStr == "" {
+			return
+		}
+
+		tokens = append(tokens, Token{Type: getTokenType(currStr), Value: currStr})
+
+		currStr = ""
+	}
+	for _, c := range sql {
+		if unicode.IsSpace(c) {
+			flush()
+			continue
+		}
+		switch c {
+		case '=':
+			flush()
+			tokens = append(tokens, Token{Type: EQ, Value: string(c)})
+		case '>':
+			flush()
+			tokens = append(tokens, Token{Type: GT, Value: string(c)})
+		case '<':
+			flush()
+			tokens = append(tokens, Token{Type: LT, Value: string(c)})
+		case ',':
+			flush()
+			tokens = append(tokens, Token{Type: COMMA, Value: string(c)})
+		case '*':
+			flush()
+			tokens = append(tokens, Token{Type: STAR, Value: string(c)})
+		default:
+			currStr += string(c)
+		}
+
+	}
+	flush()
+
+	return tokens
+}
+
+// id will be default if none is found (this may be bad assumtion?)
+func getTokenType(str string) tokenType {
+	switch strings.ToUpper(str) {
+	case "SELECT":
+		return SELECT
+	case "FROM":
+		return FROM
+	case "WHERE":
+		return WHERE
+	default:
+		if _, err := strconv.Atoi(str); err == nil {
+			return INT
+		}
+		return ID
+	}
+
+}
---
+++ pkg/sql/lex/lex_test.go
@@ -0,0 +1,127 @@
+package lex_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/aidenfine/kewl-db/pkg/sql/lex"
+)
+
+func TestLex(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected []lex.Token
+	}{
+		{
+			name:  "select",
+			input: "SELECT",
+			expected: []lex.Token{
+				{Type: lex.SELECT, Value: "SELECT"},
+			},
+		},
+		{
+			name:  "select from where",
+			input: "SELECT FROM WHERE",
+			expected: []lex.Token{
+				{Type: lex.SELECT, Value: "SELECT"},
+				{Type: lex.FROM, Value: "FROM"},
+				{Type: lex.WHERE, Value: "WHERE"},
+			},
+		},
+		{
+			name:  "identifiers",
+			input: "users accounts id",
+			expected: []lex.Token{
+				{Type: lex.ID, Value: "users"},
+				{Type: lex.ID, Value: "accounts"},
+				{Type: lex.ID, Value: "id"},
+			},
+		},
+		{
+			name:  "integer",
+			input: "10",
+			expected: []lex.Token{
+				{Type: lex.INT, Value: "10"},
+			},
+		},
+		{
+			name:  "comparison operators",
+			input: "= > <",
+			expected: []lex.Token{
+				{Type: lex.EQ, Value: "="},
+				{Type: lex.GT, Value: ">"},
+				{Type: lex.LT, Value: "<"},
+			},
+		},
+		{
+			name:  "comma and star",
+			input: ", *",
+			expected: []lex.Token{
+				{Type: lex.COMMA, Value: ","},
+				{Type: lex.STAR, Value: "*"},
+			},
+		},
+		{
+			name:  "simple select query",
+			input: "SELECT users FROM accounts",
+			expected: []lex.Token{
+				{Type: lex.SELECT, Value: "SELECT"},
+				{Type: lex.ID, Value: "users"},
+				{Type: lex.FROM, Value: "FROM"},
+				{Type: lex.ID, Value: "accounts"},
+			},
+		},
+		{
+			name:  "select with where",
+			input: "SELECT users FROM accounts WHERE id > 10",
+			expected: []lex.Token{
+				{Type: lex.SELECT, Value: "SELECT"},
+				{Type: lex.ID, Value: "users"},
+				{Type: lex.FROM, Value: "FROM"},
+				{Type: lex.ID, Value: "accounts"},
+				{Type: lex.WHERE, Value: "WHERE"},
+				{Type: lex.ID, Value: "id"},
+				{Type: lex.GT, Value: ">"},
+				{Type: lex.INT, Value: "10"},
+			},
+		},
+		{
+			name:  "operators without whitespace",
+			input: "id>10",
+			expected: []lex.Token{
+				{Type: lex.ID, Value: "id"},
+				{Type: lex.GT, Value: ">"},
+				{Type: lex.INT, Value: "10"},
+			},
+		},
+		{
+			name:  "star select",
+			input: "SELECT * FROM users",
+			expected: []lex.Token{
+				{Type: lex.SELECT, Value: "SELECT"},
+				{Type: lex.STAR, Value: "*"},
+				{Type: lex.FROM, Value: "FROM"},
+				{Type: lex.ID, Value: "users"},
+			},
+		},
+		{
+			name:  "special chars become IDS", // TODO: this should fail as of now the lexxer is not super strict
+			input: "SELECT !!!",
+			expected: []lex.Token{
+				{Type: lex.SELECT, Value: "SELECT"},
+				{Type: lex.ID, Value: "!!!"},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := lex.Lex(tt.input)
+
+			if !reflect.DeepEqual(got, tt.expected) {
+				t.Errorf("Lex(%q) = %v, expected %v", tt.input, got, tt.expected)
+			}
+		})
+	}
+}
