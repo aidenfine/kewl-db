e3211938b5a3ce3cd17b3da2b4c52132063ba02d
Aiden Fine <aiden03fine@gmail.com>
2026-09-23 23:11:36 -0400

    boilerplate code for parser

--- pkg/sql/parser/parse.go
+++ pkg/sql/parser/parse.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"errors"
+	"slices"
 	"unicode"
 	"unicode/utf8"
 
@@ -11,16 +12,43 @@ import (
 var (
 	ErrInvalidUTF8 = errors.New("UTF-8 characters are only allowed.")
 	ErrInvalidChar = errors.New("Invalid character")
+	ErrSyntaxError = errors.New("Syntax error")
 )
 
 // TODO: finish this
+// SQL statements must always start with one of the following
+// SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, DROP, TRUNCATE, GRANT, REVOKE, BEGIN, COMMIT, ROLLBACK
 func Parse(tokens []lex.Token) error {
 	// initial token checks
 	// We will run the optimizations here on the query (most likely)
 
+	// based on query type call the validator for query type.
+
 	// check if the identifiers are invalid
 	err := checkCharacters(tokens)
 	return err
+
+}
+
+func preCheck(tokens []lex.Token) error {
+	// min len checks for tokens,
+	// check if tokens end or start in a value that is not valid
+	// validate parens, could use leetcode problem valid parentheses stack method for this.
+	// After stripping all parens is the first token valid?
+
+	tokensLen := len(tokens)
+	if tokensLen < 2 {
+		return ErrSyntaxError
+	}
+
+	firstToken := tokens[0]
+	if firstToken.Value == "(" && tokensLen > 1 {
+		firstToken = tokens[1]
+	}
+	if !slices.Contains(lex.ActiveQueryTypes, firstToken.Type) {
+		return ErrSyntaxError
+
+	}
 
 }
 
---
+++ pkg/sql/parser/validator.go
@@ -0,0 +1,9 @@
+package parser
+
+import "github.com/aidenfine/kewl-db/pkg/sql/lex"
+
+// contains the validator for each query type
+
+func validateSelectQueryTokens(tokens []lex.Token) error {
+	return nil
+}
